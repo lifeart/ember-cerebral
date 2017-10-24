7e713258be0a6a21d7efde6e8e6a46e6e31e4298
lifeart <lifeart92@gmail.com>
2017-10-24 03:13:40 +0300

    addoin installation fix

--- blueprints/ember-cerebral/index.js
+++ blueprints/ember-cerebral/index.js
@@ -1,15 +1,14 @@
 /*jshint node:true*/
 module.exports = {
-  description: 'Initializer to configure and inject Cerebral into your application.'
+  description: 'Initializer to configure and inject Cerebral into your application.',
 
-  // locals: function(options) {
-  //   // Return custom template variables here.
-  //   return {
-  //     foo: options.entity.options.foo
-  //   };
-  // }
+  afterInstall: function () {
+    return this.addPackagesToProject([
+      {name: 'cerebral', target: '3.4.0'}
+    ]);
+  },
 
-  // afterInstall: function(options) {
-  //   // Perform extra work here.
-  // }
+  normalizeEntityName: function () {
+    // this prevents an error when the entityName is not specified
+  }
 };
